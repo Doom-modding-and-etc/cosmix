31fec936b8b1b3dd7638a8e775bcc278790894b1
Wolfywolfy <andregui17@outlook.com>
2022-01-31 15:48:03 -0300

    initial commit

--- source/mixer.h
+++ source/mixer.h
@@ -23,8 +23,12 @@ int  StopSampleAtChannel(int selected_channel);
 int  IsPlayingAtChannel(int chan);
 int  IsPlaying();                   // Returns the number of channels currently playing
 
-// se fizer um resume, testar se o channel.counter > 0 ... assim distingue-se entre um stop (.counter == 0) e um pause.
+/*
+**se fizer um resume, testar se o channel.counter > 0 ... assim distingue-se entre um stop (.counter == 0) e um pause.
+**If makes an resume, test if the channel.counter > 0 ... still can be distiguinted between stop (.counter = 0) and one pause
+*/
 
+///
 /// Section B
 ///
 void Mixer_Tick();
@@ -40,5 +44,7 @@ typedef struct
 
 } channel;
 
-// uso : array channels : channel channels[]
+// EN-US: use: array channels : channel channels[]
+// PT-BR uso : array channels : channel channels[]
+
 #endif
